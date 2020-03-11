c03213d0ecae39db721d6a8c37f31c09aebf8a64
shivasiddharth <ushivasiddharth@gmail.com>
2020-03-11 20:19:03 +0530

    update installer
    Update for new SDK

--- scripts/alexa-installer.sh
+++ scripts/alexa-installer.sh
@@ -17,6 +17,7 @@ echo "This could take a while grab a coffee or beer........."
 cd ${GIT_DIR}/Alexa/
 sudo chmod +x ./setup.sh
 sudo chmod +x ./pi.sh
+sudo chmod +x ./genConfig.sh
 sudo ./setup.sh
 sudo chmod +x ./test.sh
 sudo chmod +x ./startsample.sh
